8b0a4c2097b8a3ac22e485f4e2d2b6210954ab39
Anwesh43 <anweshthecool0@gmail.com>
2018-12-29 21:22:59 +0530

    adding scale related functions

--- QuarterArcStepStage.ts
+++ QuarterArcStepStage.ts
@@ -5,6 +5,26 @@ const scDiv : number = 0.51
 const strokeFactor : number = 90
 const sizeFactor : number = 2.7
 const color : string = "#43A047"
+
+const maxScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.max(0, scale - i / n)
+}
+
+const divideScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.min(1/n, maxScale(scale, i, n)) * n
+}
+
+const scaleFactor : Function = (scale : number) => Math.floor(scale / scDiv)
+
+const mirrorValue : Function = (scale : number, a : number, b : number) : number => {
+    const k : number = scaleFactor(scale)
+    return (1 - k) / a + k / b
+}
+
+const updateScale : Function = (scale : number, dir : number, a : number, b : number) : number => {
+    return mirrorValue(scale, a, b) * dir * scGap
+}
+
 class QuarterArcStepStage {
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D
